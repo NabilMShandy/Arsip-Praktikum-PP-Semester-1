7166fb6f502fd70f6323ce0609cae2aa55a0a301
NabilMShandy <nmuhammadshandy@gmail.com>
2025-12-19 22:39:58 +0700

    menambahkan semua kode

---
+++ Pertemuan 2/prak1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+string nama = "Nabil Muhamamd Shandy";
+string NPM = "2517051032";
+
+cout << "Selamat Datang Di Praktikum Pengenalan Pemrograman" << endl;
+cout << "Nama saya adalah " << nama << endl;
+cout << "NPM saya adalah " << NPM << endl << endl;
+cout << "Unila Indah" << endl;
+cout << "Be Strong" << endl;
+}
